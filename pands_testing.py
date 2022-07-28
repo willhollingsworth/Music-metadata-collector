1c2ee593ad75a309b5161a5ddfc22e5781c48e1a
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-07-28 16:36:07 +0930

    added caching to downloads

--- pands_testing.py
+++ pands_testing.py
@@ -5,7 +5,11 @@ import sp_playcount_funcs as spp
 public_url = 'https://api.t4ils.dev/'
 
 
-tracks = spp.list_tracks_from_album('4UrHb1pIbKLFev4nuxMFUY')
-df = pd.DataFrame(data=tracks)
-print('rows, columns :', df.shape)
-print(df)
+def build_df(in_data):
+    df = pd.DataFrame(data=in_data)
+    print('rows, columns :', df.shape)
+    print(df)
+
+
+if __name__ == '__main__':
+    build_df(spp.list_tracks_from_album('4UrHb1pIbKLFev4nuxMFUY'))
--- sp_playcount_funcs.py
+++ sp_playcount_funcs.py
@@ -1,16 +1,36 @@
+from turtle import down
 import requests
 import os
+import json
+
+
+def download_data(request_type, id):
+    public_url = 'https://api.t4ils.dev/'
+
+    if request_type == 'album_playcount':
+        request_url = 'albumPlayCount?albumid='
+    elif request_type == 'artist_info':
+        request_url = 'artistInfo?artistid='
+    else:
+        raise Exception('wrong type selected', locals())
+    full_path = 'cache/'+request_type+'/'+id+'.json'
+    if os.path.exists(full_path):
+        with open(full_path, 'r') as f:
+            r = json.load(f)
+    else:
+        full_url = public_url + request_url + id
+        r = requests.get(full_url)
+        r = r.json()['data']
+        with open(full_path, 'w') as f:
+            f.write(json.dumps(r))
+    return r
 
 
 def list_tracks_from_album(album_id):
-    # add caching to reduce api load + decrease repeated processing time
-    # save data locally
     output_list = []
-    public_url = 'https://api.t4ils.dev/'
-    url = public_url + 'albumPlayCount?albumid=' + album_id
-    r = requests.get(url)
-    r = r.json()['data']
-    for disc in r['discs']:
+    data = download_data('album_playcount', album_id)
+
+    for disc in data['discs']:
         for track in disc['tracks']:
             output_list.append(
                 {'Track Name': track['name'], 'Track Playcount': track['playcount']})
@@ -18,30 +38,23 @@ def list_tracks_from_album(album_id):
 
 
 def list_albums_from_artist(artist_id):
-    public_url = 'https://api.t4ils.dev/'
-    url = public_url + 'artistInfo?artistid=' + artist_id
-    r = requests.get(url)
-    r = r.json()
     output_list = []
-
-    albums = r['data']['releases']
+    r = download_data('artist_info', artist_id)
+    albums = r['releases']
     full_albums = albums['albums']['releases']
     for album in full_albums:
         album_uri = album['uri'].replace('spotify:album:', '')
+
+        if 'discs' in album.keys():
+            discs = len(album['discs'])
+        else:
+            discs = 0
         output_list.append(
-            {'Album Name': album['name'], 'Album URI': album_uri})
-        # print(album['name'],album['uri'].replace('spotify:album:',''))
-        # track_list = tracks_list + (grab_tracks_from_album(album_uri))
-        # print(album.keys())
-        # if len(album['discs']) == 0:
-        #     print(album)
-        #     continue
-        # for disc in album['discs']:
-        #     for track in disc['tracks']:
-        #         tracks_list.append(track['name'])
+            {'Album Name': album['name'], 'Album URI': album_uri, 'Discs': discs})
+
     return output_list
 
 
 if __name__ == '__main__':
     print(list_albums_from_artist('4aEnNH9PuU1HF3TsZTru54'))
-    # print(list_tracks_from_album('4UrHb1pIbKLFev4nuxMFUY'))
+    print(list_tracks_from_album('4UrHb1pIbKLFev4nuxMFUY'))
