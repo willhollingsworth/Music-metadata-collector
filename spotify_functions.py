973956a6ba4cce70b3c9ab30cc6e364bd1d9ef5f
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-07-31 18:08:41 +0930

    clearer functions and nicer search function

--- spotify_functions.py
+++ spotify_functions.py
@@ -3,7 +3,7 @@ import requests
 import utility_functions
 
 
-def create_auth_token():
+def create_headers():
     '''' load credentials via json'''
     with open('credentials.json', 'r') as r:
         credentials = json.load(r)
@@ -19,25 +19,32 @@ def create_auth_token():
     auth_response_data = auth_response.json()
     # save the access token
     access_token = auth_response_data['access_token']
-    return access_token
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=access_token)
+    }
+    return headers
 
 
 def spotify_download_data(request_type, input):
     # todo - use utility downloader to allow for local caching
 
     # https://developer.spotify.com/documentation/web-api/reference/#/
-    access_token = create_auth_token()
-    # generate header
-    headers = {
-        'Authorization': 'Bearer {token}'.format(token=access_token)
-    }
+    headers = create_headers()
     spotify_url = 'https://api.spotify.com/v1/'
     request_types = {'artists': 'artists/',
                      'albums': 'albums/',
-                     'tracks': 'tracks/',
-                     'search_artist': 'search?type=artist&q='}
+                     'tracks': 'tracks/'}
+    valid_search_types = ['artist', 'track', 'album', 'genre']
+    #  'search_artist': 'search?type=artist&q='}
     if request_type in request_types:
         request_url = request_types[request_type]
+    elif request_type.split('_')[0] == 'search':
+        search_type = request_type.split('_')[1]
+        if search_type in valid_search_types:
+            request_url = 'search?type={0}&q={0}:'.format(search_type)
+        else:
+            raise Exception(
+                'wrong search selected, only the following are valid', valid_search_types, locals())
     else:
         raise Exception('wrong type selected', locals())
     full_url = spotify_url + request_url + input
@@ -45,24 +52,65 @@ def spotify_download_data(request_type, input):
     return r.json()
 
 
+def artist_lookup(id):
+    return spotify_download_data('artists', id)
+
+
+def album_lookup(id):
+    return spotify_download_data('albums', id)
+
+
+def track_lookup(id):
+    return spotify_download_data('tracks', id)
+
+
+def search_artists(string):
+    return spotify_download_data('search_artist', string)
+
+
+def search_tracks(string, results=1):
+    if results == 1:
+        return spotify_download_data('search_track', string)['tracks']['items'][0]
+    else:
+        return spotify_download_data('search_track', string)['tracks']['items'][:results]
+
+
+def search_albums(string):
+    return spotify_download_data('search_album', string)
+
+
 if __name__ == '__main__':
 
-    print('artist example')
-    artists = spotify_download_data('artists', '3tSvlEzeDnVbQJBTkIA6nO')
-    utility_functions.print_dict_keys(
-        artists, ['name', ['followers', 'total'], 'genres'])
+    # print('artist example')
+    # artists = artist_lookup('3tSvlEzeDnVbQJBTkIA6nO')
+    # utility_functions.print_dict_keys(
+    #     artists, ['name', ['followers', 'total'], 'genres'])
 
-    print('album example')
-    albums = spotify_download_data('albums', '392RA8UhAIoBzpbn3bPy3Q')
-    utility_functions.print_dict_keys(
-        albums, ['name', 'genres', 'uri'])
+    # print('album example')
+    # albums = album_lookup('392RA8UhAIoBzpbn3bPy3Q')
+    # utility_functions.print_dict_keys(
+    #     albums, ['name', 'genres', 'uri'])
 
-    print('track example')
-    track = spotify_download_data('tracks', '6xZZM6GDxTKsLjF3TNDREL')
-    print(track['name'], '-', track['artists'][0]['name'], track['popularity'])
+    # print('track example')
+    # track = track_lookup('6xZZM6GDxTKsLjF3TNDREL')
+    # print(track['name'], '-', track['artists'][0]['name'], track['popularity'])
 
-    print('search artist example')
-    search_artist = spotify_download_data('search_artist', 'artist:pendulum')
-    print('show first 5 results - columns: name,followers,popularity,genres')
-    for x in search_artist['artists']['items'][:5]:
-        print(x['name'], x['followers']['total'], x['popularity'], x['genres'])
+    # print('search artist example')
+    # search_artists = search_artists('pendulum')
+    # print('show first 5 results - columns: name,followers,popularity,genres')
+    # for x in search_artists['artists']['items'][:5]:
+    #     print(x['name'], x['followers']['total'], x['popularity'], x['genres'])
+
+    # search_track_results = search_tracks('la danza')['tracks']['items'][0]
+
+    # utility_functions.print_dict_keys(search_track_results,
+    #                                   ['name', 'type', 'id'])
+
+    testing = search_tracks('la danza')
+    utility_functions.print_dict_keys(testing,
+                                      ['name', 'type', 'id'])
+
+    # print(testing)
+    # print(type(testing), len(testing))
+    # if isinstance(testing, dict):
+    #     print(testing.keys())
