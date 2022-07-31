1332feffee8833bd14c4a8b60561f46222715a2e
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-07-31 16:42:33 +0930

    cleanup

--- deezer_functions.py
+++ deezer_functions.py
@@ -25,38 +25,32 @@ def search(search_string, print_keys='', limit=0):
     search_data = download_deezer_data('search', search_string)
     if not limit:
         limit = len(search_data)
-    if print_keys:
-        for result in search_data[:limit]:
-            utility_functions.print_dict_keys(result, print_keys)
     return search_data[:limit]
 
 
 def album_lookup(album_id, print_keys=''):
-    album_data = download_deezer_data('album', album_id)
-    if print_keys:
-        utility_functions.print_dict_keys(album_data, print_keys)
-    return album_data
+    return download_deezer_data('album', album_id)
 
 
 def track_lookup(track_id, print_keys=''):
-    track_data = download_deezer_data('track', track_id)
-    if print_keys:
-        utility_functions.print_dict_keys(track_data, print_keys)
-    return track_data
+    return download_deezer_data('track', track_id)
 
 
 if __name__ == '__main__':
     print('string search example:')
     search_keys = ['title', ['artist', 'name'], [
         'album', 'title'], ['album', 'id'], 'type', 'id']
-    search_data = search('caribou', search_keys, 2)
+    search_data = search('caribou')
+    utility_functions.print_dict_keys(search_data[0], search_keys)
 
     print('track id lookup example:')
     track_keys = ['title', ['album', 'title'],
                   'duration', 'rank', 'bpm', 'gain', 'id']
-    track_data = track_lookup('395141722', track_keys)
+    track_data = track_lookup('395141722')
+    utility_functions.print_dict_keys(track_data, track_keys)
 
     print('album lookup example')
     album_keys = ['title', ['artist', 'name'], ['artist', 'id'],
                   'fans',  'id']
     album_data = album_lookup('46371952', album_keys)
+    utility_functions.print_dict_keys(album_data, album_keys)
