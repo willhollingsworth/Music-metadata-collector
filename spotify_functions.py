9d91224f11bd6fa420afcc7487fd31bc833cb5af
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-08-02 18:05:09 +0930

    cleanup of spotify api

--- spotify_functions.py
+++ spotify_functions.py
@@ -79,38 +79,49 @@ def search_albums(string):
     return spotify_download_data('search_album', string)
 
 
-if __name__ == '__main__':
+def return_track_details(search_string):
+    output_dict = {}
+    track_results = search_tracks(search_string)
+    output_dict['track name'] = track_results['name']
+    output_dict['track type'] = track_results['type']
+    output_dict['track id'] = track_results['id']
+    output_dict['artist name'] = track_results['artists'][0]['name']
+    output_dict['artist id'] = track_results['artists'][0]['id']
+    output_dict['album name'] = track_results['album']['name']
+    output_dict['album id'] = track_results['album']['id']
+    artist_results = artist_lookup(output_dict['artist id'])
+    output_dict['artist genres'] = artist_results['genres']
+    return output_dict
 
-    # print('artist example')
-    # artists = artist_lookup('3tSvlEzeDnVbQJBTkIA6nO')
-    # utility_functions.print_dict_keys(
-    #     artists, ['name', ['followers', 'total'], 'genres'])
 
-    # print('album example')
-    # albums = album_lookup('392RA8UhAIoBzpbn3bPy3Q')
-    # utility_functions.print_dict_keys(
-    #     albums, ['name', 'genres', 'uri'])
+def example_uses():
+    print('artist example')
+    artists = artist_lookup('3tSvlEzeDnVbQJBTkIA6nO')
+    utility_functions.print_dict_keys(
+        artists, ['name', ['followers', 'total'], 'genres'])
 
-    # print('track example')
-    # track = track_lookup('6xZZM6GDxTKsLjF3TNDREL')
-    # print(track['name'], '-', track['artists'][0]['name'], track['popularity'])
+    print('album example')
+    albums = album_lookup('2dIGnmEIy1WZIcZCFSj6i8')
+    print(albums.keys())
+    utility_functions.print_dict_keys(
+        albums, ['name', 'genres', 'uri'])
 
-    # print('search artist example')
-    # search_artists = search_artists('pendulum')
-    # print('show first 5 results - columns: name,followers,popularity,genres')
-    # for x in search_artists['artists']['items'][:5]:
-    #     print(x['name'], x['followers']['total'], x['popularity'], x['genres'])
+    print('track example')
+    track = track_lookup('6xZZM6GDxTKsLjF3TNDREL')
+    print(track['name'], '-', track['artists'][0]['name'], track['popularity'])
 
-    # search_track_results = search_tracks('la danza')['tracks']['items'][0]
+    print('search artist example')
+    artist_results = search_artists('pendulum')
+    print('show first 5 results - columns: name,followers,popularity,genres')
+    for x in artist_results['artists']['items'][:5]:
+        print(x['name'], x['followers']['total'], x['popularity'], x['genres'])
 
-    # utility_functions.print_dict_keys(search_track_results,
-    #                                   ['name', 'type', 'id'])
-
-    testing = search_tracks('la danza')
-    utility_functions.print_dict_keys(testing,
+    search_track_results = search_tracks('la danza')
+    utility_functions.print_dict_keys(search_track_results,
                                       ['name', 'type', 'id'])
 
-    # print(testing)
-    # print(type(testing), len(testing))
-    # if isinstance(testing, dict):
-    #     print(testing.keys())
+
+if __name__ == '__main__':
+
+    testing = (return_track_details('loud pipes'))
+    print(testing)
