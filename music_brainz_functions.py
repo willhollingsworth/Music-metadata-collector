b183c6c36907486768e03f7034abf0da0ef37c36
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-05-06 17:33:13 +0930

    MB adding recording listing

--- music_brainz_functions.py
+++ music_brainz_functions.py
@@ -1,6 +1,7 @@
 import requests
 import utility_functions
 import json
+from pprint import pprint
 '''
     https://musicbrainz.org/doc/MusicBrainz_API
     https://musicbrainz.org/doc/MusicBrainz_API/Examples
@@ -9,14 +10,16 @@ import json
 
 def music_brainz_download_data(url_args, overwrite=0, debug=0):
     base_url = 'https://musicbrainz.org/ws/2/'
-    url_args += '&fmt=json'
+    if '?' in url_args:
+       url_args += '&fmt=json'
+    else:
+        url_args += '?fmt=json'
     return utility_functions.download_data(base_url + url_args)
 
 
 def search_artist(artist):
     # https://musicbrainz.org/doc/MusicBrainz_API/Search
     return music_brainz_download_data('artist?query='+artist)
-
 
 def search_track(track, artist=''):
     if not artist:
@@ -25,23 +28,47 @@ def search_track(track, artist=''):
         url = 'recording?query="{}" AND artist:"{}"'.format(track, artist)
     return music_brainz_download_data(url)
 
+def lookup_artist_id(id):
+    # https://musicbrainz.org/doc/MusicBrainz_API/Search
+    return music_brainz_download_data('artist/'+id)
 
-def run_examples():
-    artist = 'dynoro'
+def lookup_releases(id):
+    # example url https://musicbrainz.org/ws/2/release?artist=f2c454ec-69a2-49a7-a79c-eaabef25ba44
+    return music_brainz_download_data(f'release?artist={id}&inc=release-groups')
+
+
+
+def run_search_examples():
+    artist = 'Joris Delacroix'
     print('search for artist:', artist)
     artists = search_artist(artist)
-    print([a['name']+' ' + a['type']+' ' + a['id']
-          for a in (artists)['artists']])
 
+    results = ([f"name: {a['name']}, id: {a['id']}, score: {a['score']}"
+          for a in (artists)['artists']])
+    for x in results[:1]:
+        print(x)
     track, track_artist = ('satisfaction', 'benny benassi')
     print('search for track:', track, 'by artist:', track_artist)
     tracks = search_track(track, track_artist)
-    print(tracks['recordings'][0]['title'], 'by',
-          tracks['recordings'][0]['artist-credit'][0]['name'])
+    recording = tracks['recordings'][0]
+    print(recording['title'], 'by',
+          recording['artist-credit'][0]['name'])
 
     # utility_functions.save_to_file(
     #     json.dumps(tracks, indent=1), 'response.json')
 
+def run_chain_example(artist):
+    results = search_artist(artist)
+    artist_id = results['artists'][0]['id']
+    print(artist_id)
+    artist_details = lookup_artist_id(artist_id)
+    # print()
+    releases = lookup_releases(artist_id)
+    # pprint(releases)
+    release_details = [f"{r['title']} - {r['release-group']['primary-type']}" for r in releases['releases']]
+    pprint(release_details)
 
+    return
 if __name__ == '__main__':
-    run_examples()
+    # run_search_examples()
+    run_chain_example('Joris Delacroix')
