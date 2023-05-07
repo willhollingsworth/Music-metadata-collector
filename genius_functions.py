722ff51f8c47273ba2a36412193022d9695d595b
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-05-07 20:21:45 +0930

    further work on genius funcs

--- genius_functions.py
+++ genius_functions.py
@@ -1,19 +1,22 @@
 import utility_functions
 from pprint import pprint
+from collections import Counter
 
 # example https://melaniewalsh.github.io/Intro-Cultural-Analytics/04-Data-Collection/07-Genius-API.html
 
 
 def genius_download_data(input):
     credentials = utility_functions.load_credentials('genius')
-    cred_arg = '&access_token=' + credentials['access_token']
+    if '?' in input:
+        cred_arg = '&access_token=' + credentials['access_token']
+    else:
+        cred_arg = '?access_token=' + credentials['access_token']
     genius_api_url = 'https://api.genius.com/'
-
     full_url = genius_api_url + input + cred_arg
+
     return utility_functions.download_data(full_url)
 
 def search(track):
-    # https://www.last.fm/api/show/track.search
     arg = 'search?q='
     return genius_download_data(arg + track)['response']['hits']
 
@@ -22,9 +25,54 @@ def format_search_results(results):
     for i in results:
         out.append('{} by {}'.format(i['result']['title'],i['result']['artist_names']))
     return out
+
+def get_artist(artist_id):
+    arg = 'artists/'
+    return genius_download_data(arg + artist_id)
+
+def get_artist_songs(artist_id,page=1,songs_only=True):
+    per_arg = 'artists/'
+    post_arg = '/songs?per_page=50&page=' + str(page)
+    # return genius_download_data(arg + artist_id +'/songs?per_page=50')
+    if songs_only:
+        return genius_download_data(per_arg + artist_id + post_arg )['response']['songs']
+    else:
+        return genius_download_data(per_arg + artist_id + post_arg )
+
+def get_all_songs(artist_id):
+    page = 1
+    songs = []
+    while True:
+        response = get_artist_songs(artist_id,page,songs_only=False)
+        songs += response['response']['songs']
+        if response['response']['next_page'] == None:
+            break
+        page += 1
+    return songs
+
+def run_tests():
+    # run search
+    search_string = 'lane 8'
+    results = search(search_string)
+    print('searching for string \"{}\", found {} results'.format(search_string,len(results)))
+    # pprint(format_search_results(results)) # show results 
+    # list artist ids from search    
+    print('artist ids of results :',dict(Counter([x['result']['primary_artist']['id'] for x in results])))
+    # lists all songs from artist id
+    results = get_all_songs(artist_id='582604')
+    print('get all songs: found {} songs by {}'.format(len(results),results[0]['artist_names']))
+    # print(['{} by {}'.format(song['title'],song['artist_names']) for song in results])
+
+
 if __name__ == '__main__':
-    # print(utility_functions.show_structure(search('dynoro')[0]))
-    results = search('rock it by sub focus')
-    results = format_search_results(results)
-    pprint(results)
-    # print('{} by {}'.format(result['title'],result['primary_artist']['name']))
+    run_tests()
+    # results = search('sub focus')
+    # pprint(get_artist('100291'))
+    # results = get_all_songs(artist_id='100291')
+    # print(type(results),len(results))
+    # print(['{} by {}'.format(song['title'],song['artist_names']) for song in results])
+    # print(results.keys())
+    # print(format_search_results(results))
+    # utility_functions.print_structure(results[0])
+    # pprint(results[3]['result']['primary_artist']['id'])
+
