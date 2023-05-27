9cb751daa62772289973df164f204fd63a2b5463
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-05-27 17:49:55 +0930

    better error handling

--- overall.py
+++ overall.py
@@ -4,15 +4,37 @@ import music_brainz_functions
 from pprint import pprint
 import utility_functions as utils
 
-if __name__ == '__main__':
+def grab_current_track_artist():
     spotify_playing = spotify_functions.current_playing()['item']
-    current_playing_track = spotify_playing['name']
-    current_playing_artist = spotify_playing['artists'][0]['name']
+    out_dict ={}
+    out_dict['track'] = spotify_playing['name']
+    out_dict['artist'] = spotify_playing['artists'][0]['name']
+
+    return out_dict
+
+
+if __name__ == '__main__':
+    spotify_results = grab_current_track_artist()
+    current_playing_track,current_playing_artist = spotify_results['track'],spotify_results['artist']
+    # current_playing_track,current_playing_artist = ('Trouble','Robby East')
     print('Spotify currently playing :', current_playing_track,'by',current_playing_artist)
-    dez_track = deezer_functions.search(track=current_playing_track,artist=current_playing_artist,detailed=True)
-    print('Deezer album genres :',dez_track['genres'])
+
+    
+    print()
     mb_track = music_brainz_functions.search_track(current_playing_track,current_playing_artist)
-    if 'genres' in mb_track.keys():
-        print('Music Brainz genres :',mb_track['genres'])
+    if not isinstance(mb_track, dict):
+        print('Music Brainz found no results')
     else:
-        print('Music Brainz found no genres')
+        if 'genres' in mb_track.keys():
+            if mb_track['genres'][0] != 'none':
+                print('Music Brainz genres :',mb_track['genres'])
+            else: 
+                print('Music Brainz found no genres')
+        else:
+            print('Music Brainz found no genres')
+    dez_track = deezer_functions.search(track=current_playing_track,artist=current_playing_artist,detailed=True)
+    # print(len(dez_track['genres']))
+    if len(dez_track['genres']) > 1:
+        print('Deezer album genres :',dez_track['genres'])
+    else:
+        print('Deezer found no genres')
