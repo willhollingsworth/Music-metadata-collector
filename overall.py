9d26bd7fd7e536d87211508ed82bc1ae90705bee
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-05-29 20:38:02 +0930

    fixed folder structure

--- overall.py
+++ overall.py
@@ -1,21 +1,24 @@
-import spotify_functions
-import deezer_functions
-import music_brainz_functions
+# import Services.spotify_functions as spotify_functions
+import MMC.Services.spotify as spotify
+# from MMC.Services.spotify_functions import 
+# import spotify_functions
+import MMC.Services.deezer as deezer
+# import music_brainz_functions
 from pprint import pprint
-import utility_functions as utils
+import MMC.Util.utility as utils
 
 if __name__ == '__main__':
-    # utils.delete_cache()
-    spotify_current_id = spotify_functions.current_playing()
+    utils.delete_cache()
+    spotify_current_id = spotify.current_playing()
     #spotify
-    spotify_track = spotify_functions.lookup_track_detailed(spotify_current_id)
+    spotify_track = spotify.lookup_track_detailed(spotify_current_id)
     current_playing_track,current_playing_artist = spotify_track['track name'],spotify_track['artist name']
     print('Spotify currently playing :', current_playing_track,'by',current_playing_artist)
-    print('Spotify artist genres :',spotify_track['artist genres'])
+    print('Spotify results      artist genres :',spotify_track['artist genres'])
     #deezer
-    dez_search = deezer_functions.search_track(track=current_playing_track,artist=current_playing_artist)
-    dez_details = deezer_functions.lookup_track_detailed(dez_search['track id'])
-    print(  f'deezer results for {dez_details["track name"]} by {dez_details["artist name"]}',
+    dez_search = deezer.search_track(track=current_playing_track,artist=current_playing_artist)
+    dez_details = deezer.lookup_track_detailed(dez_search['track id'])
+    print(  f'deezer results for {dez_details["track name"]} by {dez_details["artist name"]}    ',
             f'album genres : {dez_details["album genres"]}',
     )
     # print(len(dez_track['genres']))
