a7aa47b98c173a11d52907ea7088f46409d306cd
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-06-19 17:01:10 +0930

    setting up inits

---
+++ MMC/Core/__init__.py
@@ -0,0 +1,3 @@
+from .metadata_collector import get_current_track_details
+
+__all__ = ["get_current_track_details"]
---
+++ MMC/Services/__init__.py
@@ -0,0 +1,17 @@
+from .beatport import *
+from .deezer import *
+from .genius import *
+from .last_fm import *
+from .music_brainz import *
+from .spotify import *
+from .spotify_playcount_functions import *
+
+__all__ = [
+    'beatport',
+    'deezer',
+    'genius',
+    'last_fm',
+    'music_brainz',
+    'spotify',
+    'spotify_playcount_functions',
+]
