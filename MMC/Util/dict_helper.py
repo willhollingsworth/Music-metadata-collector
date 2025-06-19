97e241409c2009d5f17c97c963a3ae2f53548bfe
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-06-19 19:58:50 +0930

    doc: improved dict help docs

--- MMC/Util/dict_helper.py
+++ MMC/Util/dict_helper.py
@@ -5,7 +5,11 @@ def map_dict_keys(
     input_dict: dict[str, Any],
     mapping_rules: list[tuple[str, str | list[str]]],
 ) -> dict[str, str]:
-    """Map and transform dictionary keys based on provided rules."""
+    """Map and transform dictionary keys based on provided rules.
+
+    rule format is (new key, old key)
+    if old key is a list then it will traverse the input dict
+    """
     output_dict: dict[str, str] = {}
     for new_key, old_key_path in mapping_rules:
         if not isinstance(old_key_path, list):
