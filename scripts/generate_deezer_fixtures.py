1ba587e56ff7df8c2f429a6d53b11033453aae4a
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-06-21 16:08:12 +0930

    refactor: moved fixture gen into single function, also load ids from json

--- scripts/generate_deezer_fixtures.py
+++ scripts/generate_deezer_fixtures.py
@@ -1,57 +1,42 @@
 import json
 from pathlib import Path
 from typing import Any
-from MMC.Services.deezer import download_deezer_data, lookup_track, get_first_track, lookup_album
+import MMC.Services.deezer as deezer
 
 FIXTURE_DIR = Path('tests/fixtures/deezer/')
+EXPECTED_FILENAME_PREFIX = '_expected'
+LOOKUP_FUNCTION_SUFFIX = 'lookup_'
 
-def generate_track_fixtures(track_id: str) -> None:
-    """Generate json response for a Deezer track lookup."""
-    track = download_deezer_data('track', track_id)
-    if isinstance(track, list):
-        track = get_first_track(track)
-    path = Path('tracks') / f'{track_id}.json'
-    write_json_fixture(track, path)
-
-def generate_track_fixtures_expected(track_id: str) -> None:
-    """Generate expected fixture for a track."""
-    track = lookup_track(track_id)
-    path = Path('tracks') / f'{track_id}_expected.json'
-    write_json_fixture(track, path)
-
-def generate_album_fixtures(album_id: str) -> None:
-    """Generate json response for a Deezer album lookup."""
-    album = download_deezer_data('album', album_id)
-    path = Path('albums') / f'{album_id}.json'
-    write_json_fixture(album, path)
-
-def generate_album_fixtures_expected(album_id: str) -> None:
-    """Generate expected fixture for an album."""
-    album = lookup_album(album_id)
-    path = Path('albums') / f'{album_id}_expected.json'
-    write_json_fixture(album, path)
+def generate_deezer_fixtures(type: str, id: int) -> None:
+    # Generate a fixture from Deezer's API response.
+    api_response = deezer.download_deezer_data(type, str(id))
+    if isinstance(api_response, list):
+        api_response = deezer.get_first_track(api_response)
+    path = FIXTURE_DIR / type / f'{id}.json'
+    write_json_fixture(api_response, path)
+    print(f'Fixture written to {path}')
+    # Generate expected outcome from the lookup function.
+    lookup_function = getattr(deezer, f'{LOOKUP_FUNCTION_SUFFIX}{type}', None)
+    if lookup_function is None:
+        raise ValueError(f'No lookup function found for "{LOOKUP_FUNCTION_SUFFIX}{type}"')
+    expected_response = lookup_function(str(id))
+    expected_path = FIXTURE_DIR / type / f'{id}{EXPECTED_FILENAME_PREFIX}.json'
+    write_json_fixture(expected_response, expected_path)
+    print(f'Fixture written to {expected_path}')
 
 def write_json_fixture(data: dict[str, Any], path: Path) -> None:
     """Write a JSON fixture to a file."""
-    full_path = FIXTURE_DIR / path
-    with open(full_path, 'w') as f:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, 'w') as f:
         json.dump(data, f, indent=2)
 
 if __name__ == '__main__':
-    track_ids = [
-        '2582922142',  
-        '2582922152',
-    ]
-    
-    album_ids = [
-        '523121232',
-        '7933290'
-    ]
-
-    for track_id in track_ids:
-        generate_track_fixtures(track_id)
-        generate_track_fixtures_expected(track_id)
-
-    for album_id in album_ids:
-        generate_album_fixtures(album_id)
-        generate_album_fixtures_expected(album_id)
+    # open the deezer_fixture_ids.json as json object
+    with open('scripts/deezer_fixture_ids.json') as file:
+        ids = json.load(file)
+        for type, id_list in ids.items():
+            for id in id_list:
+                print(f'Generating fixture for {type} {id}')
+                generate_deezer_fixtures(type, id)
+    print('All fixtures generated successfully.')
+        
