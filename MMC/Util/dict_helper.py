8c0b8070213d43aa88768806e495d95fd78c33fc
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-06-23 19:04:39 +0930

    added util for traversing nested dicts + misc code cleanup

--- MMC/Util/dict_helper.py
+++ MMC/Util/dict_helper.py
@@ -1,3 +1,5 @@
+"""Dictionary helper functions."""
+
 from typing import Any
 
 
@@ -21,24 +23,33 @@ def map_dict_keys(
         output_dict[new_key] = value
     return output_dict
 
+
+def get_nested(nested_dict: dict[str, Any], keys: list[str] | str) -> Any:
+    """Traverse nested dictionaries using a list of keys."""
+    if isinstance(keys, str):
+        # adds support for non nested keys
+        return nested_dict[keys]
+    for key in keys:
+        nested_dict = nested_dict[key]
+    return nested_dict
+
+
 if __name__ == "__main__":
     mapping_rules = [
-        ('track name', 'title'),
-        ('track id', 'id'),
-        ('artist name', ['artist', 'name']),
-        ('artist id', ['artist', 'id']),
-        ('album name', ['album', 'title']),
-        ('album id', ['album', 'id']),
+        ("track name", "title"),
+        ("track id", "id"),
+        ("artist name", ["artist", "name"]),
+        ("artist id", ["artist", "id"]),
+        ("album name", ["album", "title"]),
+        ("album id", ["album", "id"]),
     ]
     test_dict = {
-        'title': 'Test Song',
-        'id': '1',
-        'artist': {
-            'name': 'Test Artist',
-            'id': '2'},
-        'album': {
-            'title': 'Test Album',
-            'id': '3',
+        "title": "Test Song",
+        "id": "1",
+        "artist": {"name": "Test Artist", "id": "2"},
+        "album": {
+            "title": "Test Album",
+            "id": "3",
         },
     }
     result = map_dict_keys(test_dict, mapping_rules)
