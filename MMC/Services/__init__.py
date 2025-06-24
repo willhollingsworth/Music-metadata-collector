8ea13190fb9b1979e7d01d07e3bca26bbd857310
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-06-24 20:16:57 +0930

    refactor: moved spotify logic to it's own folder

--- MMC/Services/__init__.py
+++ MMC/Services/__init__.py
@@ -3,15 +3,15 @@ from .deezer import *
 from .genius import *
 from .last_fm import *
 from .music_brainz import *
-from .spotify import *
+from .spotify.spotify import *
 from .spotify_playcount_functions import *
 
 __all__ = [
-    'beatport',
-    'deezer',
-    'genius',
-    'last_fm',
-    'music_brainz',
-    'spotify',
-    'spotify_playcount_functions',
+    "beatport",
+    "deezer",
+    "genius",
+    "last_fm",
+    "music_brainz",
+    "spotify",
+    "spotify_playcount_functions",
 ]
