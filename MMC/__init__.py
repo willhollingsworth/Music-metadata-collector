f86972512ef0d8e7abfe3ddc0e41253b4c8dad80
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-06-25 16:05:18 +0930

    add deezer lookups to main package

--- MMC/__init__.py
+++ MMC/__init__.py
@@ -1,5 +1,8 @@
 """Music Metadata Collector (mmc) package."""
 
+from mmc.services.deezer.lookups import lookup_album as deezer_lookup_album
+from mmc.services.deezer.lookups import lookup_artist as deezer_lookup_artist
+from mmc.services.deezer.lookups import lookup_track as deezer_lookup_track
 from mmc.services.spotify.lookups import lookup_album as spotify_lookup_album
 from mmc.services.spotify.lookups import lookup_artist as spotify_lookup_artist
 from mmc.services.spotify.lookups import lookup_track as spotify_lookup_track
@@ -7,6 +10,9 @@ from mmc.services.spotify.status import current_playing as spotify_current_playi
 from mmc.utils.cache import delete_cache
 
 __all__ = [
+    "deezer_lookup_album",
+    "deezer_lookup_artist",
+    "deezer_lookup_track",
     "delete_cache",
     "spotify_current_playing",
     "spotify_lookup_album",
