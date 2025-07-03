b1262c0c5ccf843ae28af4ac908bd3f01e2a191b
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-07-03 17:40:07 +0930

    refactor: moved deezer search logic into a class

--- MMC/Services/deezer/searches.py
+++ MMC/Services/deezer/searches.py
@@ -1,64 +1,78 @@
 """Searches using Deezer API."""
 
-from typing import Any
-
+from mmc.constants import DEEZER_API_URL
 from mmc.models.deezer_models import Track
-from mmc.services.deezer.api_requests import request_search
-from mmc.services.deezer.utils import get_first_track
+from mmc.utils.http_client import download_json
+from mmc.utils.list_helper import find_first_matching_dict
+
+SERVICE_NAME = "deezer"
 
 
-def build_search_args(
-    search_string: str = "",
-    artist: str = "",
-    track: str = "",
-) -> str:
-    """Build the search arguments for Deezer."""
-    search_items: list[str] = []
-    if bool(search_string):
-        search_items.append(search_string)
-    if bool(artist):
-        search_items.append(f'artist:"{artist}"')
-    if bool(track):
-        search_items.append(f'track:"{track}"')
-    return " ".join(search_items)
+class Search:
+    """Performs searches using the Deezer API for tracks, artists, or albums."""
+
+    def __init__(
+        self,
+        search_type: str = "",
+        artist: str = "",
+        track: str = "",
+        album: str = "",
+    ) -> None:
+        """Initialize the search with the given parameters."""
+        self.search_type = search_type
+        self.artist = artist
+        self.track = track
+        self.album = album
+        self.service_name = SERVICE_NAME
+        self.search_url = self.build_search_url()
+
+    def build_search_url(self) -> None:
+        """Build the search arguments for Deezer."""
+        search_items: list[str] = []
+        if self.artist:
+            search_items.append(f'artist:"{self.artist}"')
+        if self.track:
+            search_items.append(f'track:"{self.track}"')
+        if self.album:
+            search_items.append(f'album:"{self.album}"')
+        self.search_string = " ".join(search_items)
+
+    def run(self) -> Track:
+        """Run the search and return the results.
+
+        Raises:
+            ValueError: If the search type is not supported.
+
+        """
+        search_url = DEEZER_API_URL + "search?q=" + self.search_string
+        result = download_json(search_url, SERVICE_NAME, "search", self.search_string)
+        if isinstance(result, list):
+            result = find_first_matching_dict(
+                result,
+                "type",
+                self.search_type,
+            )
+        if self.search_type == "track":
+            return Track.from_dict(result)
+        msg = f"Search type {self.search_type} is not supported."
+        raise ValueError(msg)
 
 
-def search_deezer(
-    search_string: str = "",
-    artist: str = "",
-    track: str = "",
-) -> dict[str, Any]:
-    """Search for a track on Deezer using a string.
-
-    Raises:
-        TypeError: If the search result is not a dictionary.
-
-    """
-    search_string_final = build_search_args(search_string, artist, track)
-    search_data = request_search(search_string_final)
-    if isinstance(search_data, list):
-        search_data = search_data[0]
-    if isinstance(search_data, dict):
-        return search_data
-    msg = f"Expected a dict, got {type(search_data)}"
-    raise TypeError(msg)
-
-
-def search_track(
-    search_string: str = "",
-    artist: str = "",
-    track: str = "",
-) -> Track:
-    """Search for a track on Deezer and return its details."""
-    result = search_deezer(search_string, artist, track)
-    return Track.from_dict(get_first_track(result))
+def search_track_examples() -> None:
+    """Run some search examples."""
+    print("Running Deezer search examples...")
+    track = "no freedom"
+    print(f"searching {track=}")
+    print(Search(search_type="track", track=track).run())
+    artist = "angara"
+    print()
+    print(f"searching {track=}, {artist=}")
+    print(Search(search_type="track", track=track, artist=artist).run())
+    print()
+    album = "prove"
+    print(f"searching {track=}, {album=}")
+    print(Search(search_type="track", track=track, album=album).run())
 
 
 if __name__ == "__main__":
-    """Run the deezer examples."""
-    print("Running Deezer examples...")
-    print()
-    track = "la danza"
-    print("string search for track:", track, end=", result =  ")
-    print(search_track(track))
-    print()
+    search_track_examples()
